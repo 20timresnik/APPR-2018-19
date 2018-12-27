5734911225cea7682e981ef08a1a60979419bf9b
20timresnik <resnik.tim.hbk@hotmail.com>
2018-12-27 13:17:53 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -10,3 +10,14 @@ delezi.tidy <- melt(delezi1, id.vars="Dejavnosti", measure.vars=names(delezi1)[-
                     variable.name="Kvartali",value.name="Stalne cene, referenčno leto 2010, v mio €", na.rm=TRUE)
 
 #delezi.tidy$Kvartali <- parse_integer(delezi.tidy$Kvartali)
+
+evropaA <- read_csv("evropaA.csv", skip=1, col_names=c('Kvartal', 'Drzava', 'Enota', 'Sezona', 'Dejavnost', 'Merilo',
+                                               'Stalne cene, referenčno leto 2010, v mio €','Dodatno'), na=":",locale=locale(encoding="Windows-1250"))
+
+evropaA$Enota <- NULL
+evropaA$Sezona <- NULL
+evropaA$Merilo <- NULL
+evropaA$Dodatno <- NULL
+
+evropaB <- evropaA[,c("Drzava","Dejavnost","Kvartal","Stalne cene, referenčno leto 2010, v mio €")]
+
