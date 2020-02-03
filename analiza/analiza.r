e444469af33e80cb45307b30d3237ca8e4451035
20timresnik <resnik.tim.hbk@hotmail.com>
2020-02-03 19:40:10 +0100

    Dodana napoved BDP

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,30 @@
+library(dplyr)
+library(tidyr)
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+library(reshape2)
+library(ggplot2)
+
+
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+#Izračun podatkov za napoved
+podatki.napoved <- bdp.evropa %>% filter(Drzava == 'Slovenia', Leto %in% 1995:2017) %>%
+  transmute(Leto, BDP=BDP*1e6/Vrednost)
+fit <- lm(data = podatki.napoved, BDP ~ Leto)
+a <- data.frame(Leto=seq(2018, 2025))
+predict(fit, a)
+napoved <- a %>% mutate(BDP=predict(fit, .))
+
+
+#Graf napovedi
+graf9 <- ggplot(podatki.napoved, aes(x=Leto, y=BDP)) +
+  theme(axis.title=element_text(size=11), plot.title=element_text(size=15, hjust=0.5)) +
+  geom_col(data=napoved, aes(x=Leto, y=BDP), fill="grey43", alpha=0.6, width = 0.6, colour="grey0") +
+  labs(title="Napoved BDP Slovenija", y="BDP/prebivalca") +
+  geom_col(fill="springgreen4", width = 0.6, colour="grey0") +
+  geom_smooth(method=lm, colour = "red", linetype = "11", size=2, fullrange = TRUE, se=FALSE)
+
